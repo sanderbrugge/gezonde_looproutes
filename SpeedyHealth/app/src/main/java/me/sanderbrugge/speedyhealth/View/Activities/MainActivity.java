fa3f26a080935b613cdb5984d54ed78cd575d8bb
Sander Brugge <sander.brugge@moovly.com>
2018-03-17 11:43:12 +0100

    support for polylines

--- SpeedyHealth/app/src/main/java/me/sanderbrugge/speedyhealth/View/Activities/MainActivity.java
+++ SpeedyHealth/app/src/main/java/me/sanderbrugge/speedyhealth/View/Activities/MainActivity.java
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import java.util.ArrayList;
 
 import me.sanderbrugge.speedyhealth.Model.Looproutes;
@@ -22,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    loadMap(map);
+                }
+            });
+        } else {
+            Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
+        }
         looproutes = new ArrayList<>();
         getLooproutes();
     }
@@ -44,4 +69,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void loadMap(GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(-18.142, 178.431), 2));
+
+        // Polylines are useful for marking paths and routes on the map.
+        googleMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(new LatLng(-33.866, 151.195))  // Sydney
+                .add(new LatLng(-18.142, 178.431))  // Fiji
+                .add(new LatLng(21.291, -157.821))  // Hawaii
+                .add(new LatLng(37.423, -122.091))  // Mountain View
+        );
+    }
+
 }
